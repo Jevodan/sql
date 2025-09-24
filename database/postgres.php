b1e6a1279f9ace4d15cef15be2b1bc54ea4b8524
Jevodan <Jevodan@users.noreply.github.com>
2025-09-24 14:52:44 +0300

    Update postgres.php

--- database/postgres.php
+++ database/postgres.php
@@ -1,6 +1,6 @@
 <?php
-const DB_HOST = 'localhost';
-const DB_PORT = '5432';
-const DB_NAME = 'postgres';
-const DB_USERNAME = 'postgres';
-const DB_PASSWORD = 'postgres';
+const DB_HOST = getenv('DB_HOST');
+const DB_PORT = getenv('DB_PORT');
+const DB_NAME = getenv('DB_NAME');
+const DB_USERNAME = getenv('DB_USERNAME');
+const DB_PASSWORD = getenv('DB_PASSWORD');
